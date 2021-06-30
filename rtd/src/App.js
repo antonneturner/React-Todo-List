be4d74cb4740d74e4786aed79222288da164b7b8
winfrom within <antonneturner@gmail.com>
2021-06-30 00:43:17 -0400

    add onclick event handler to componets

--- rtd/src/App.js
+++ rtd/src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
 //  componet imports
 
 import Form from "./componets/Form";
 import TodoList from "./componets/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
       <TodoList />
     </div>
   );
