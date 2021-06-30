78f6a719df064e1aee4bc923d733dedcf1d47685
winfrom within <antonneturner@gmail.com>
2021-06-30 00:13:09 -0400

    add font awesome plus sign to the todo list

--- rtd/src/App.js
+++ rtd/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+//  componet imports
 
+import Form from "./componets/Form";
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
+      <Form />
     </div>
   );
 }
