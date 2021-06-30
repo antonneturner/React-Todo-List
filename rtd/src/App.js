75a87e8e61fba693044085e6e61ddc8fbb07fd6a
winfrom within <antonneturner@gmail.com>
2021-06-30 00:19:49 -0400

    export todolist and form componets

--- rtd/src/App.js
+++ rtd/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 //  componet imports
 
 import Form from "./componets/Form";
+import TodoList from "./componets/TodoList";
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
         <h1>Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
